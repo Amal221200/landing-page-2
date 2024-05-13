1b51d60d39239c5417e66aaca1add3020d20440e
Amal221200 <amalmurikkoli2000@gmail.com>
2024-05-13 10:01:05 +0530

    m

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -3,7 +3,7 @@ import { Button } from '../ui/button'
 import { ArrowRight } from 'lucide-react'
 import { cn } from '@/lib/utils'
 
-const PrimaryButton = ({ children, className }: { children: React.ReactNode, className: string }) => {
+const PrimaryButton = ({ children, className }: { children: React.ReactNode, className?: string }) => {
     return (
         <Button type="button" variant="primary-gradient" className={cn('flex gap-x-1', className)}>
             {children} <ArrowRight size={18} />
--- src/components/buttons/SecondaryButton.tsx
+++ src/components/buttons/SecondaryButton.tsx
@@ -3,7 +3,7 @@ import { Button } from '../ui/button'
 import { ArrowRight } from 'lucide-react'
 import { cn } from '@/lib/utils'
 
-const SecondaryButton = ({ children, className }: { children: React.ReactNode, className: string }) => {
+const SecondaryButton = ({ children, className }: { children: React.ReactNode, className?: string }) => {
     return (
         <Button type="button" variant="secondary" className={cn('flex gap-x-1', className)}>
             {children} <ArrowRight size={18} />
